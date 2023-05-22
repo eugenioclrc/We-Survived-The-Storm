33a5180cdf3e91ea0a28932b5693fbe6bf449689
moiseiggy <103082031+moiseiggy@users.noreply.github.com>
2023-05-22 14:56:03 -0500

    WeSurvivedTheStorm.sol

--- src/WeSurvivedTheStorm.sol
+++ src/WeSurvivedTheStorm.sol
@@ -22,9 +22,13 @@ contract WeSurvivedTheStorm is ERC721 {
     constructor(address _owner) ERC721() {
         owner = _owner;
         _mint(_owner, 1);
+        tokenDescriptions[1] = "The first song in the collection from “We Survived The Storm” The inspiration behind this piece and the music came from the natural escape that sports can provide for people through their day-to-day lives. As we get caught up in work, relationships, and our passions, keeping a healthy lifestyle and finding balance is essential. In this image, the sport of choice is basketball.";
         _mint(_owner, 2);
+        tokenDescriptions[2] = "The second song in the collection from “We Survived The Storm” The inspiration behind this piece and the music came from the natural nourishment that fruits can provide for people through their day-to-day lives. As we get caught up in work, relationships, and our passions, we must keep a healthy lifestyle and find balance in the foods we consume. In this image, the fruit of choice is raspberry.";
         _mint(_owner, 3);
+        tokenDescriptions[3] = "The third song in the collection from “We Survived The Storm” The inspiration behind this piece and the music came from the pursuit of taking risks and decisions that people make daily. As we get caught up in work, relationships, and our passions, we must keep pursuing our dreams no matter how far out they may be. In this image, characters are jumping in parachutes through this stormy foreground.";
         _mint(_owner, 4);
+        tokenDescriptions[4] = "The fourth song in the collection from “We Survived The Storm” The inspiration behind this piece and the music came from the feeling of having your back against the wall, and it’s up to you to keep pushing to get back up on your feet. As we get caught up in work, relationships, and our passions, it is essential to not let yourself ever get too down, surround yourself with people who believe in you, and continue to be resilient when times get tough. This image has a paper plane shaped like a heart soaring across the sky.";
     }
 
     /// @notice ERC2981, Returns the royalty amount for a given NFT and sale price
